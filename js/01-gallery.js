bf7a5a2e70a2fa96c8364d7f0a7883d6e491fd0c
AdrianZ96 <adrianzdziarski96@wp.pl>
2023-09-11 13:06:26 +0200

    Update 01-gallery.js
    1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -4,25 +4,31 @@ import { galleryItems } from './gallery-items.js';
 console.log(galleryItems);
 
 const gallery = document.querySelector('ul.gallery')
+ 
+
+gallery.addEventListener("click", (event) => {
+    if (event.target.tagName === "IMG") {
+        const picture = galleryItems.find(item => item.preview === event.target.src);
+        const lightbox = basicLightbox.create(`
+            <img src="${picture.original}" alt="${picture.description}">
+        `);
+
+        lightbox.show();
+        document.addEventListener("keydown", (event) => {
+            if (event.key === "Escape" || event.key === "Esc") {
+                lightbox.close();
+            }
+        });
+     }
+    });
+
 
 galleryItems.forEach(picture => {
     const item = document.createElement('li');
     item.insertAdjacentHTML(
     'beforeend',
     `<img src="${picture.preview}" alt="${picture.description}" width="340" height="240">`);
+    
     gallery.appendChild(item);
 
-    item.addEventListener("click", () => {
-    
-    const lightbox = basicLightbox.create(`
-            <img src="${picture.original}" alt="${picture.description}">
-        `);
-
-        lightbox.show();
-        document.addEventListener("keydown", (event) => {
-            if (event.key === "Escape"|| event.key ==="Esc") {
-                lightbox.close();
-            }
-        });
-    });
 });
