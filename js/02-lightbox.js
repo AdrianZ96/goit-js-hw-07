2f4dad0cb56a484ab77d93e9a3861267c96e3f21
AdrianZ96 <adrianzdziarski96@wp.pl>
2023-09-11 08:50:53 +0200

    1
    1

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,32 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from './gallery-items.js'; 
 
-console.log(galleryItems);
+document.addEventListener('DOMContentLoaded', function () {
+    const galleryList = document.querySelector('.gallery');
+
+   
+    galleryItems.forEach((item) => {
+        const listItem = document.createElement('li');
+        listItem.classList.add('gallery__item');
+
+        const link = document.createElement('a');
+        link.classList.add('gallery__link');
+        link.href = item.original;
+
+        const image = document.createElement('img');
+        image.classList.add('gallery__image');
+        image.src = item.preview;
+        image.alt = item.description;
+
+        link.appendChild(image);
+        listItem.appendChild(link);
+        galleryList.appendChild(listItem);
+    });
+
+    
+    const lightbox = new SimpleLightbox('.gallery a', {
+        captionsData: 'alt',
+        captionDelay: 250,
+        captionPosition: 'bottom',
+    });
+
+});
